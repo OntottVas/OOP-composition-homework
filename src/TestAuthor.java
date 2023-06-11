f9864049bc9883367f4c78fa09a967a154e5473f
Geree <smithzero94@gmail.com>
2023-06-11 10:43:06 +0200

    Author class defined, Tests are done according README.md file in the TestAuthor class's main.

--- src/TestAuthor.java
+++ src/TestAuthor.java
@@ -1,2 +1,11 @@
 public class TestAuthor {
+    public static void main(String[] args) {
+        Author kFollet = new Author("Ken Follet", "ken@follet.org", 'm');
+
+        System.out.println(kFollet);
+        kFollet.setEmail("follet@ken.org");
+        System.out.println("Name: " + kFollet.getName());
+        System.out.println("Email: " + kFollet.getEmail());
+        System.out.println("Gender: " + kFollet.getGender());
+    }
 }
